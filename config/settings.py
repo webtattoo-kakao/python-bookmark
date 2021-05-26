1bf5eebf4dc3dfbc461733addf49f7c65cf3a317
PARKKYUTAE <webtattoo@kakao.com>
2021-05-26 23:55:49 +0900

    정적파일 설정 정보

--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+
+
+
